41cd4b09f3dd0a4c570e62dc9ab75da8c210388e
unknown <tsea@WITPK08K2M.wit.private>
2014-10-31 22:11:24 -0400

    something

--- src/p2pchat/Client.java
+++ src/p2pchat/Client.java
@@ -1,5 +1,14 @@
 package p2pchat;
 
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
 public class Client {
 
 }
--- src/p2pchat/ServerHandler.java
+++ src/p2pchat/ServerHandler.java
@@ -1,6 +1,5 @@
 package p2pchat;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.ReferenceCountUtil;
@@ -10,18 +9,14 @@ import io.netty.util.ReferenceCountUtil;
  */
 public class ServerHandler extends ChannelInboundHandlerAdapter { // (1)
 
-    @Override
-    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
-    	 ByteBuf in = (ByteBuf) msg;
-    	    try {
-    	        while (in.isReadable()) { // (1)
-    	            System.out.print((char) in.readByte());
-    	            System.out.flush();
-    	        }
-    	    } finally {
-    	        ReferenceCountUtil.release(msg); // (2)
-    	    }
-    }
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+	    try {
+	        // Do something with msg
+	    } finally {
+	        ReferenceCountUtil.release(msg);
+	    }
+	}
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
